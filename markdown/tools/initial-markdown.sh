a45695af31cd7dbcddc935b2c1a18310e6bd0c45
zhou <63698158+kita-zhou@users.noreply.github.com>
2026-02-02 18:16:26 +0800

    [BugFix][Markdown] fix copyright
    add copyright for markdown tools script
    
    issue: #0

--- markdown/tools/initial-markdown.sh
+++ markdown/tools/initial-markdown.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# Copyright 2026 The Lynx Authors. All rights reserved.
+# Licensed under the Apache License Version 2.0 that can be found in the
+# LICENSE file in the root directory of this source tree.
+
 pushd markdown/third_party/lynx-textra
 source tools/envsetup.sh
 ./tools/hab sync .
