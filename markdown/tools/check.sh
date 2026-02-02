69c4616f293d7e96c5d4bdad1da9f41bc043b9a4
zhou <63698158+kita-zhou@users.noreply.github.com>
2026-02-02 18:16:26 +0800

    [BugFix][Markdown] fix copyright
    add copyright for markdown tools script
    
    issue: #0

--- markdown/tools/check.sh
+++ markdown/tools/check.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+# Copyright 2026 The Lynx Authors. All rights reserved.
+# Licensed under the Apache License Version 2.0 that can be found in the
+# LICENSE file in the root directory of this source tree.
+
 source tools/envsetup.sh
 python3 tools_shared/git_lynx.py check --checkers file-type,cpplint,java-lint,commit-message,coding-style,android-check-style
