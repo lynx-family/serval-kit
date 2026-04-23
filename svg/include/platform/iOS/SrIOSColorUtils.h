c370059d2b8e1f24a1e8a3db768c2fdebe76fc35
keweibing <756938814@qq.com>
2026-04-23 15:30:39 +0800

    [BugFix] add copyright

--- svg/include/platform/iOS/SrIOSColorUtils.h
+++ svg/include/platform/iOS/SrIOSColorUtils.h
@@ -1,3 +1,6 @@
+// Copyright 2024 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
 #ifndef SVG_INCLUDE_PLATFORM_IOS_SRIOSCOLORUTILS_H_
 #define SVG_INCLUDE_PLATFORM_IOS_SRIOSCOLORUTILS_H_
 
